7454d5d8c92a41b9d44737b771e3e7571e01e363
codewithnavinya <navinyavarute@gmail.com>
2025-07-06 19:05:40 +0530

    Add files via upload

---
+++ average_of_2_number.py
@@ -0,0 +1,6 @@
+#Enter your first number
+a=int(input('Enter your first number:'))
+#Enter your second number
+b=int(input('Enter your secomd number:'))
+#your answer is
+print('YOUR AVERAGE IS:', (a+b)/2)
